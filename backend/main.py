420b957a3751e4e8f9a535780859a518a21babd6
Udita Singh <udita1294@gmail.com>
2026-08-06 19:42:48 +0530

    Resume text parsed

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,34 @@
+import json
+import os
+from pathlib import Path
+from dotenv import load_dotenv
 from fastapi import FastAPI
+from groq import Groq
+from pydantic import BaseModel
+from pypdf import PdfReader
+
+load_dotenv()
+
 
 app = FastAPI()
 
+# pdf extraction
+def read_pdf(file_path: Path):
+    reader = PdfReader(file_path)
+    text = ""
+    for page in reader.pages:
+        page_text = page.extract_text()
+        if page_text:
+            text += page_text + "\n"
+    return text
+
+
 @app.get("/")
 def home():
+    resume_text = read_pdf(Path("my_resume.pdf"))
+    print(resume_text)
     return {
-        "message" : "HireMe API is running successfully!"
+        "message" : "Resume parsed successfully!"
     }
+
+
